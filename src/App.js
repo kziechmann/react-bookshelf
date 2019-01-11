3dac1f581b30985e5d4600771354245a0f9177c6
kziechmann <kziechmann@gmail.com>
2019-01-11 15:30:25 -0700

    Formatted  booklist on home page and got appropriate data to display from each Item.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Cart from './components/Cart';
+import BookList from './components/BookList';
 import './App.css';
 
 class App extends Component {
@@ -21,7 +21,7 @@ class App extends Component {
 
   render() {
     return (<div>
-                  <Cart books={this.state}/>
+                  <BookList books={this.state}/>
             </div>)
   }
 
