5724d4e75eb0f7ccf8291a5bd5c21bb73fe033af
kziechmann <kziechmann@gmail.com>
2019-01-11 14:10:33 -0700

    Created basic react-app and componentDidMount function to retrieve books json data from server.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+  state={
 
+  }
+
+  componentDidMount = async() => {
+    const response = await fetch('http://localhost:8082/api/books', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    })
+    const books = await response.json()
+    this.setState(books)
+  }
+
+  render() {
+    return <div>{JSON.stringify(this.state)}</div>
+  }
+
+}
 export default App;
